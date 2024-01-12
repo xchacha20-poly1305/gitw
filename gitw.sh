5b19945024bffc078b7e37a311a56ee1ec51df59
HystericalDragon <HystericalDragons@proton.me>
2024-01-12 22:58:21 +0800

    Add squash option
    Signed-off-by: HystericalDragon <HystericalDragons@proton.me>

--- gitw.sh
+++ gitw.sh
@@ -27,9 +27,13 @@ custom_help() {
     echo "    commita     git commit --amend"
     echo "    sync        force sync remote"
     echo "    clean       clean git reflog"
+    echo "    squash [HEAD]      squash some commits"
     echo
     echo "options:"
     echo "    options for git"
+    echo
+    echo "HEAD:"
+    echo "    git hader, like HEAD^ or sha1"
 
     exit 0
 }
@@ -71,6 +75,22 @@ clean() {
     exit 0
 }
 
+squash() {
+    if [ -z $(git branch --show-current) ]; then
+        echo "You are not one a branch"
+        exit 1
+    fi
+
+    unsafe_notice
+
+    TARGET_COMMIT=$1
+    NOW_COMMIT=$(git rev-parse HEAD)
+
+    git reset --hard $TARGET_COMMIT
+    git merge $NOW_COMMIT --squash
+    git commit -s
+}
+
 ############################# start
 
 main() {
@@ -82,6 +102,7 @@ main() {
     [ "$1" == "commita" ] && commita $OTHER_OPTIONS
     [ "$1" == "sync" ] && sync $OTHER_OPTIONS
     [ "$1" == "clean" ] && clean
+    [ "$1" == "squash" ] && squash $2
 
     git $@
 }
